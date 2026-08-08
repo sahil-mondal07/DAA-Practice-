f4bfe434e52e66ebfc51a0f1b93fbc7a21e0c88c
sahil-mondal07 <mondalsahil591@gmail.com>
2026-08-08 14:38:07 +0530

    Initial commit from local workspace

---
+++ DAY1_armstrong.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>//power function ke liye math module include kar rhe hai
+
+int main(){
+    int n, temp, digit = 0, sum = 0, rem;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    temp = n; //no. of digit count hoga 
+    while(temp != 0){
+        digit++;
+        temp = temp / 10;
+    } 
+    temp = n; //jitna digit count hoga uska power har ek no. pe lagake add hoga
+    while(temp != 0){
+        rem = temp % 10;             //kisi no. se last digit nikalne ke liye ek ek karke 
+        sum += (int)pow(rem, digit); 
+        temp = temp / 10;//last digit cutoff karke baki ka no, deta hai
+    }
+
+    if(sum == n)
+        printf("%d is an Armstrong number.\n", n);
+    else
+        printf("%d is NOT an Armstrong number.\n", n);
+
+    return 0;
+}
