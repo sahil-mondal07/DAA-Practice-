e70534f6058c529b00d26bbaf074b94cdcf7fc36
sahil-mondal07 <mondalsahil591@gmail.com>
2026-08-08 15:34:28 +0530

    Add lab1q2.c

---
+++ lab1q2.c
@@ -0,0 +1,20 @@
+// Write a program to count the frequency of each distinct element in an array.
+#include<stdio.h>
+int main(){
+    int a[]={2,5,2,67,10,5,67,2,10};
+    int size=sizeof(a)/sizeof(a[0]);
+    int count =0;
+    for(int i=0;i<size;i++){
+        count =1 ;
+        for(int j=i+1;j<size;j++){
+            if(a[i]==a[j]){
+                count++;
+                a[j]=-1;
+            }
+        }
+        if(a[i]!=-1){
+            printf("Element %d occurs %d times\n", a[i], count);
+        }
+    }
+    return 0;
+}
