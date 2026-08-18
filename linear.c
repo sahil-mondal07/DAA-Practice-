a3e2e8d77fc0ff206a3a9f1cf1427b3fa954a222
sahil-mondal07 <mondalsahil591@gmail.com>
2026-08-18 10:48:12 +0530

    linear search with time

--- linear.c
+++ linear.c
@@ -1,5 +1,6 @@
-//time comparison of binary and linear search 
-
+//time comparison of linear search 
+#include<stdlib.h>
+#include<time.h>
 #include <stdio.h>
 int linearSearch(int arr[], int size, int target)
 {
@@ -12,12 +13,39 @@ int linearSearch(int arr[], int size, int target)
     }
     return -1;
 }
-int main()
-{
-    int arr[] = {2,10,3,45,31,77,56,29};
-    int size = 8;
-    int target = 31;
+int main(){
+    int size=10000;
+    int arr[10000];
+    //yha pe random 10k inputs generate hoga
+    srand(time(NULL));
+    for(int i=0;i<size;i++){
+        arr[i]=rand()%1000000;
+    }
+    //yhe pe taget search hoga 
+    int target=arr[size-1];
+     int target = arr[size - 1];
+
+    clock_t start, end;
+    double time_taken;
+
+    start = clock();
+
     int result = linearSearch(arr, size, target);
-    printf("Index = %d", result);
+
+    end = clock();
+
+    time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+
+    if (result != -1)
+    {
+        printf("Element found at index = %d\n", result);
+    }
+    else
+    {
+        printf("Element not found\n");
+    }
+
+    printf("Time taken = %f seconds\n", time_taken);
+
     return 0;
 }
