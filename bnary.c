f845ab8dc2d0be9a3453af91796a564fad1cfaf4
Sahil Mondal <mondalsahil591@gmail.com>
2026-08-22 22:56:49 +0530

    Update bnary.c

--- bnary.c
+++ bnary.c
@@ -1,9 +1,12 @@
-//time comparison of binary and linear search
+//time comparison of binary search
 #include <stdio.h>
+#include <time.h>
+
 int binarySearch(int arr[], int size, int target)
 {
     int low = 0;
     int high = size - 1;
+
     while (low <= high)
     {
         int mid = low + (high - low) / 2;
@@ -21,14 +24,29 @@ int binarySearch(int arr[], int size, int target)
             high = mid - 1;
         }
     }
+
     return -1;
 }
+
 int main()
 {
-    int arr[] = {2,10,23,45,56};
+    int arr[] = {2, 10, 23, 45, 56};
     int size = 5;
     int target = 23;
+
+    clock_t start, end;
+    double time_taken;
+
+    start = clock();
+
     int result = binarySearch(arr, size, target);
-    printf("Index = %d", result);
+
+    end = clock();
+
+    time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("Index = %d\n", result);
+    printf("Time taken = %f seconds\n", time_taken);
+
     return 0;
 }
